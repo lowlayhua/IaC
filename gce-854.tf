bc76e4f6bd8d12e8f27dddd3767f03f94c837d24
Low Lay Hua <44797521+lowlayhua@users.noreply.github.com>
2024-07-13 13:04:22 +0800

    Create gce-854.tf

---
+++ gce-854.tf
@@ -0,0 +1,61 @@
+resource "google_compute_instance" "quickstart-vm" {
+  boot_disk {
+    auto_delete = true
+    device_name = "quickstart-vm"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240709"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-small"
+
+  metadata = {
+    enable-oslogin = "true"
+  }
+
+  name = "quickstart-vm"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/qwiklabs-gcp-04-92f1288b8e35/regions/us-west1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "545797514456-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "us-west1-c"
+}
